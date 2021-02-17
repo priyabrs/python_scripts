635c11c3e1f9fbfb0e4b327329cd552f6d08308b
Priyabrata <samal7244@gmail.com>
2021-02-17 22:28:58 +0530

    exception handling

--- nato_alphabet/nato_alphabet.py
+++ nato_alphabet/nato_alphabet.py
@@ -8,6 +8,7 @@ for (key, value) in student_dict.items():
     #Access key and value
     pass
 
+from itsdangerous import exc
 import pandas as pd
 student_data_frame = pd.DataFrame(student_dict)
 
@@ -26,7 +27,13 @@ nato_alphabet_dict = {}
 for _index, row in nato_alphabet_df.iterrows():
     nato_alphabet_dict[row.letter] = row.code
 # print(nato_alphabet_dict)
-user_name = input('Enter your name: ')
-print([nato_alphabet_dict[char] for char in user_name.upper()])
+retry = True
+while retry:
+    try:
+        user_name = input('Enter your name: ')
+        print([nato_alphabet_dict[char] for char in user_name.upper()])
+        retry = False
+    except KeyError:
+        print('Only letters are allowed')
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
