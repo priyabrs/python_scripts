224d9fcfc0dd03494fe0c9cd53cec2a0796fd29e
Priyabrata <samal7244@gmail.com>
2021-02-25 17:39:13 +0530

    adding params to api call

--- api/api_quiz/data.py
+++ api/api_quiz/data.py
@@ -1,9 +1,6 @@
 import requests
 
-param = {
-    'amount': 10,
-    'type' : 'boolean'
-}
+param = { 'amount': 10, 'type' : 'boolean'}
 web_response = requests.get(url='https://opentdb.com/api.php', params=param)
 web_response.raise_for_status()
 data = web_response.json()
